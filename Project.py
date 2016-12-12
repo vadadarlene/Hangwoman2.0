9c8b046327020fa0b11541eebeb63a45dc9d7264
Vada Garbutt (VDGARB8467) <VDGARB8467@ung.edu>
2016-12-12 16:46:43 -0500

    second commit
    commit final project

--- Project.py
+++ Project.py
@@ -10,27 +10,27 @@ while restart!="x":
     import random 
     f = open("words.txt", 'r')
     wordbank = [line.strip() for line in f.readlines()]
-    f.close()
-
-
+    f.close() #inputs random word
 
     word=random.choice(wordbank)
-    original=list(word)
+    original=list(word.upper())
     guess=[]
     guesses=int(0) 
-    user=''
-    change=int(0)
+    user='' # user input
+    change=int(0) #position of the character in the word
     space=list(word)
     alph=('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
 
 
     for char in range(len(original)): 
         if (original[char]=='A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'):
-            guess.append('_')
+            guess.append('_') # creates the spaces per letters
 
     print guess
 
-    while guesses<8:
+
+
+    while guesses<10:
         user=str.upper(raw_input('Guess : '))
 
         if len(user)>1: 
@@ -45,31 +45,33 @@ while restart!="x":
 
 
         if user in original:
-            while user in space:
-                change=space.index(user)
-                space.remove(user)
-                space.insert(change,'_')
-            change = 0
 
-            for char in range(0(space)):
+            while user in space:
+                guess.remove(change,'_')
+                input(user) in change
+
+                
+            for user in range(len(original)):
                 if space[char]=='_':
                     change+=1
 
-            if change==len(original):
-                print 'Correct', guess
+            if change==len(original): 
+                print 'Correct!!', guess
                 print 'You Win !'
-                guesses=8
+                guesses=10
                 break
+            
 
-            print 'Correct' , guess , 'Guesses left: ', (8-guesses)
+            print 'Correct!' , guess , 'Guesses left: ', (10-guesses)
 
         else:
             guesses+=1
-            print 'Incorrect', 'Guesses left: ', (8-guesses)
+            print 'Incorrect!', 'Guesses left: ', (10-guesses)
     else:
-        print 'You Lose !'
-
-        print 'Correct answer was', original
+         print 'You Lose !'
+         print 'Correct answer was', original
 
     input("press '1 + ENTER' to play again, exit out to be done.")
 
+                
+                
